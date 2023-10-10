b2b88c102542295bb1fc01b7757532354a5a9ede
Eloy Leiva López <118524344+eleilop@users.noreply.github.com>
2023-10-10 13:56:15 +0200

    Add files via upload

---
+++ EJ03/src/App.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un día del 1 al 7: ");
+        int dia = sc.nextInt();
+        sc.close();
+        switch (dia) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println(dia+" no corresponde a un día de la semana");
+        }
+    }
+}
